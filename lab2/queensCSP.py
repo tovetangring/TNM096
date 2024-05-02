627f41247ed7cdab7272362aba244fa1824fae4a
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-05-02 11:00:29 +0200

    queensCSP fixed n value

--- lab2/queensCSP.py
+++ lab2/queensCSP.py
@@ -15,7 +15,7 @@ def now():
     
 
 # 1. Set up the problem and starting time
-n = 5
+n = 4
 
 print("\nStarting at at  "+now()[12:20])
 print("problem with n =",n)
@@ -24,9 +24,9 @@ start = time()
 problem = NQueensCSP(n)
 
 # 2. Solve the problem
-solution = backtracking_search(problem)
+# solution = backtracking_search(problem)
 #solution = AC3(problem); 
-#solution = min_conflicts(problem)
+solution = min_conflicts(problem)
 
 
 # 3. Print the results
